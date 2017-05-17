88f893222834b3de33105ae14160a078876c0ce3
georgesuveti <george.suveti@irian.ro>
2017-05-17 17:11:45 +0300

    add scrolled class to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+declare var $: any;
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
+@Component(
+  {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+  }
+)
+export class AppComponent implements OnInit {
+
   title = 'app works!';
+
+  ngOnInit(): void {
+    if ($('.navbar').length > 0) {
+      $(window).on(
+        "scroll load resize",  ()=> {
+          this.checkScroll();
+        }
+      );
+    }
+
+
+  }
+
+  checkScroll() {
+    //var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+    if ($(window).scrollTop() > 100) {
+      $('.navbar').addClass("scrolled");
+    } else {
+      $('.navbar').removeClass("scrolled");
+    }
+  }
+
 
 }
